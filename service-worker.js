b49d05848b18876b9b546fc8f33359c1f8d4facd
Perustream <micasaperu21@gmail.com>
2025-06-23 01:02:39 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('radio-cache-v1').then(cache => {
+    caches.open("radio-cache-v1").then(cache => {
       return cache.addAll([
-        './radio_junto_a_ti_pwa.html',
-        './manifest.json'
+        "./radio_junto_a_ti_lazy_sin_botones_visibles_abajo.html",
+        "./manifest.json"
       ]);
     })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
